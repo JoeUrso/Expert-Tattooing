5615119532344deffff7115b75875a54263f38d1
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-04 14:25:51 -0700

    Type the citybutton city prop

--- client/src/components/CityButton/CityButton.tsx
+++ client/src/components/CityButton/CityButton.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./CityButton.scss";
 
-export default function CityButton({ city }) {
-    return <Link to="/">{city}</Link>;
+interface IProps {
+    city: string;
+}
+
+export default function CityButton({ city }: IProps) {
+    return (
+        <Link to="/" className="city-button">
+            {city}
+        </Link>
+    );
 }
