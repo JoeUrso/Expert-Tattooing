54f3a20f92c513d9464dd3864a37843476dd56ed
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-05 15:40:19 -0700

    Update description and hero text

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
         <main className="homepage">
             <section className="homepage__hero">
                 <p className="homepage__hero-text">
-                    We Help You Find the Finest American Traditional Tattooing
+                    We Help You Find the Finest Traditional Tattooing
                     Establishments and Artists In Your Area
                 </p>
             </section>
@@ -19,17 +19,16 @@ export default function HomePage() {
                     {`Tattooing has grown rapidly over the last couple decades -
                     meaning more styles, more artists, and more tattoo parlours.
                     Maybe you think that's great, maybe you don't. Either way,
-                    what you should think is that the American Traditional style
+                    what you should think is that the Traditional style
                     of tattooing deserves to be respected as a time tested,
                     badass, staple of the tattoo community.`}
                 </p>
                 <p className="homepage__description-text">
                     {`That's why we made Expert Tattooing. Our mission is to help
-                    you find local tattoo artists who excel in the American
-                    Traditional style. We want you to know you'll be spending
+                    you find local tattoo artists who excel in one of the many Traditional styles. We want you to know you'll be spending
                     your hard earned money on the best of the best. Looking for
-                    geometric shapes or hyper realistic superhero tattoos? This
-                    site is not for you. Looking for eagles, skulls, or roses?
+                    geometric shapes or hyper-realistic superhero tattoos? This
+                    site is not for you. Looking for eagles? Skulls? Dragons? Lady Heads?
                     Welcome, let's find you an expert.`}
                 </p>
             </section>
