8f2c1a4b315f594b34f76228eadb93940c8bdd97
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-07 19:29:20 -0700

    Set up server boilerplate and serve script

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,14 @@
 //TODO You set the main folder at the rootDir in tsconfig, fix if it has issues
+import express, { Application, NextFunction, Request, Response } from "express";
+
+// Boot express
+const app: Application = express();
+const port = 8000;
+
+// Application routing
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ data: "Hello from Ornio AS" });
+});
+
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
