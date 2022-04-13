846985ea4545670f5165198492bd65b368b8fa1b
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-12 17:52:08 -0700

    Install types for cors

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,19 @@
 //TODO You set the main folder at the rootDir in tsconfig, fix if it has issues
+import cors from "cors";
 import * as dotenv from "dotenv";
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
+
 const app: Application = express();
 
 // IMPORT FROM ENV
 dotenv.config({ path: __dirname + "/.env" });
 const port = process.env.PORT || 8080;
 
+// ENABLE CORS
+app.use(cors());
+
 // Application routing
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
+app.use("/", (req: Request, res: Response) => {
     res.status(200).send({ data: "Hello from Ornio AS" });
 });
 
