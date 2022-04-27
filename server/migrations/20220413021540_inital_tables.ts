aaa730a815b53141f665f946a3f4fc30e528f48c
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-27 15:35:22 -0700

    Begin writing migration

--- server/migrations/20220413021540_inital_tables.ts
+++ server/migrations/20220413021540_inital_tables.ts
@@ -1,10 +1,38 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema
+        .createTable("cities", (table) => {
+            table.increments("id").primary();
+            table.string("name").notNullable();
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        })
+        .createTable("shops", (table) => {
+            table.increments("id").primary();
+            table.string("name").notNullable();
+            table
+                .integer("city_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("cities")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        })
+        .createTable("artists", (table) => {
+            table.increments("id").primary();
+            table.string("name").notNullable();
+            table
+                .integer("shop_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("shops")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        });
 }
 
-
-export async function down(knex: Knex): Promise<void> {
-}
-
+export async function down(knex: Knex): Promise<void> {}
