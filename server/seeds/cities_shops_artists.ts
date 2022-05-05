0d76634d85f675e689db41f2d579ff3eb3d2156c
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-05-05 12:27:00 -0700

    Update imports in seed file

--- server/seeds/cities_shops_artists.ts
+++ server/seeds/cities_shops_artists.ts
@@ -1,7 +1,7 @@
 import { Knex } from "knex";
-const citiesData = require("../seed_data/cities");
-const shopsData = require("../seed_data/shops");
-const artistsData = require("../seed_data/artists");
+import artistsData from "../seed_data/artists";
+import citiesData from "../seed_data/cities";
+import shopsData from "../seed_data/shops";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
