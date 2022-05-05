b2cb9c052fe408856797456434da69a074493b43
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-05-05 13:02:06 -0700

    Create boilerplate object for shops seed data

---
+++ server/seed_data/shops.ts
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: 1,
+        name: 2,
+        city_id: 1,
+        address: "",
+        contact_info: {
+            phone: "",
+            email: "",
+            other: "",
+        },
+        website: "",
+        hours: {
+            monday: "",
+            tuesday: "",
+            wednesday: "",
+            thursday: "",
+            friday: "",
+            saturday: "",
+            sunday: "",
+        },
+        about: "",
+        location: "",
+        instagram: "",
+    },
+];
