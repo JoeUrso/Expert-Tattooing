6a9940349ffafaf2eab4e931deb75a6416b2ed0d
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-05-09 14:56:32 -0700

    Fix seed file and run

--- server/seeds/cities_shops_artists.ts
+++ server/seeds/cities_shops_artists.ts
@@ -1,16 +1,24 @@
+// TODO Figure out how to make this work properly with ESlint/TS so you're not using require statements?
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Knex } from "knex";
-import artistsData from "../seed_data/artists";
-import citiesData from "../seed_data/cities";
-import shopsData from "../seed_data/shops";
+// import { citiesData } from "../seed_data/cities";
+// import { shopsData } from "../seed_data/shops";
+// import { artistsData } from "../seed_data/artists.js";
+
+// const artistsData = require("../seed_data/artists");
+const shopsData = require("../seed_data/shops");
+const citiesData = require("../seed_data/cities");
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("cities").del();
     await knex("shops").del();
-    await knex("artists").del();
+    // await knex("artists").del();
 
     // Inserts seed entries
     await knex("cities").insert(citiesData);
     await knex("shops").insert(shopsData);
-    await knex("artists").insert(artistsData);
+    // await knex("artists").insert(artistsData);
 }
