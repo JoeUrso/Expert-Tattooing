a2eb451885332e07211bdfff4030cbc881326d45
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-05-11 16:45:24 -0700

    Update cities controller and citiesRoutes to get cities properly

--- server/controllers/citiesController.ts
+++ server/controllers/citiesController.ts
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// TODO enable the above and fix the issues the right way
 
-const knex = require("knex")(require("../../knexfile").development);
 import { Request, Response } from "express";
+const knex = require("knex")(require("../../knexfile").development);
 
 interface Cities {
     id: number;
@@ -14,13 +14,27 @@ interface Cities {
     updated_at: string;
 }
 
-// SEND CITIES TABLE
-exports.index = (_req: Request, res: Response) => {
-    knex("cities")
-        .then((data: Cities) => {
-            res.status(200).json(data);
-        })
-        .catch((err: string) =>
-            res.status(400).send(`Error retrieving rappers: ${err}`)
-        );
-};
+class CitiesController {
+    async read(req: Request, res: Response) {
+        const cities: Cities = await knex("cities").select("*");
+
+        return res.status(200).json(cities);
+    }
+}
+
+export { CitiesController };
+
+// TODO this was an attempt to do it the way you did in bootcamp, delete?
+// const knex = require("knex")(require("../../knexfile").development);
+// import { Request, Response } from "express";
+
+// // SEND CITIES TABLE
+// exports.index = (_req: Request, res: Response) => {
+//     knex("cities")
+//         .then((data: Cities) => {
+//             res.status(200).json(data);
+//         })
+//         .catch((err: string) =>
+//             res.status(400).send(`Error retrieving rappers: ${err}`)
+//         );
+// };
--- server/routes/citiesRoutes.ts
+++ server/routes/citiesRoutes.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+// TODO enable the above and fix the issues the right way
 import { Router } from "express";
+import { CitiesController } from "../controllers/citiesController";
 
 const router = Router();
 
-router.route("/").get(citiesController.index);
+const citiesController = new CitiesController();
+
+router.route("/").get(citiesController.read);
 
 export default router;
